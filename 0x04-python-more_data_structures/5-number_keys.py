acf0c6a2a6e304aa4a01da4b42b7132787056963
GArdennes <kevin.afachao@gmail.com>
2023-08-16 09:25:25 -0700

    Task 5 Number of keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,3 @@
 #!/usrs/bin/python3
 def number_keys(a_dictionary):
-    key_list = []
-    for key in a_dictionary.keys():
-        key_list.append(key)
-    return len(key_list)
+    return len(a_dictionary)
