043973fc462db07738c16dc9d027c62fb254423f
GArdennes <kevin.afachao@gmail.com>
2023-08-16 09:09:42 -0700

    Task 0 Squared Simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix[:]
-    new_matrix = list(map(lambda x: x ** 2, new_matrix))
-    return new_matrix
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
