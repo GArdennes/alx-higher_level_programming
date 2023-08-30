c939e64ef1fb874dce9b1e5a8db18bfe0456961b
GArdennes <kevin.afachao@gmail.com>
2023-08-30 12:01:58 -0700

    Tasks

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines an empty class: square"""
+
+
+class Square:
+    """Represents a square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a class:square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """
+        Initializes a new square
+
+        Args:
+        size (int): new square size
+        """
+        self.__size = size
