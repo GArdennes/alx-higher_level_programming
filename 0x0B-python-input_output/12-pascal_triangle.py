efdbcdfc2966deca4e5e0deaaf6c14dfc2e53b02
GArdennes <kevin.afachao@gmail.com>
2023-09-12 12:16:35 -0700

    Task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Task 12
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    triangle = []
+    for row in range(n):
+        current_row = []
+
+        for j in range(row + 1):
+            if j == 0 or j == row:
+                current_row.append(1)
+            else:
+                previous_row = triangle[row - 1]
+                current_value = previous_row[j - 1] + previous_row[j]
+                current_row.append(current_value)
+
+        triangle.append(current_row)
+    return triangle
