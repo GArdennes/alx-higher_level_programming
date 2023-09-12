946b080a5b7654e82aca625ec274ce8f81d6cc8d
GArdennes <kevin.afachao@gmail.com>
2023-09-12 11:45:44 -0700

    Task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Task 7
+"""
+
+
+import sys
+import os
+
+
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file = __import__(
+        '6-load_from_json_file.py').load_from_json_file
+
+my_list = []
+if os.path.exists("add_item.json"):
+    my_list = load_from_json_file("add_item.json")
+
+my_list.extend(sys.argv[1:])
+save_to_json_file(my_list,  "add_item.json")
