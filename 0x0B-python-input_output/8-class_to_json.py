4565cdc44b517018fa702459351b4b79840e13a3
GArdennes <kevin.afachao@gmail.com>
2023-09-12 11:51:24 -0700

    Task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Task 8
+"""
+
+
+def class_to_json(obj):
+    """
+    Return the dictionary description with simple data structure
+    Args:
+        obj: the object to serialize
+    Returns:
+        The dictionary description of the object
+    """
+    if isinstance(obj, list):
+        return obj.__dict__
