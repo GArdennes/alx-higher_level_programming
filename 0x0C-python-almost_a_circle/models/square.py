fd9eb15b81ae534bccd920fcb0bce4f16ed73391
GArdennes <thekevin.afachao@gmail.com>
2023-09-19 05:20:43 -0700

    Task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,24 +2,23 @@
 """
 Rectangle Object
 """
-
-
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """Define Square class"""
-
     def __init__(self, size, x=0, y=0, id=None):
         """Class constructor"""
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
+        """size getter"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """size setter"""
         self.width = value
         self.height = value
 
