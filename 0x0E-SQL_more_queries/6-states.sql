64b1b6066656caaf833f61cd8365e150d92ca77e
GArdennes <kevin.afachao@gmail.com>
2023-10-18 13:04:16 -0700

    Task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- A script that creates the database ‘hbtn_0d_usa’
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+id INT UNIQUE NOT NULL AUTO_INCREMENT,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY (id)
+);
+
