996f968e9a8a1a9f1e5d6b665cd0382105563707
GArdennes <kevin.afachao@gmail.com>
2023-11-14 17:28:41 +0000

    Advanced task

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // function that executes x times a function.
 
-exports.multi_c = function (x, theFunction) {
+exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i++) {
     theFunction();
   }
