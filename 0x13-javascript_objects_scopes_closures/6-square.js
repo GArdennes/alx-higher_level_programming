49180fe46f55868de89e6c0a65fceff127aaa20e
GArdennes <kevin.afachao@gmail.com>
2023-11-14 18:16:25 +0000

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// class Square that defines a square and inherits from Rectangle of 4-rectangle.js
+
+const SquareBase = require('./5-square');
+
+class Square extends SquareBase {
+  charPrint (c) {
+    for (let i = 0; i < this.size; i++) {
+      let row = '';
+      for (let j = 0; j < this.size; j++) {
+        if (c === undefined) {
+          row += 'X';
+        } else {
+          row += c;
+        }
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Square;
