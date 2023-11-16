63aad3e5d2cf159560bc99e3ee1bd85bc2c33f8c
GArdennes <kevin.afachao@gmail.com>
2023-11-16 11:14:35 -0800

    Task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+python script that lists all states from a database
+"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """
+    Function for listing states in ascending order
+    """
+    if len(sys.argv) != 5:
+        sys.exit(1)
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_n = sys.argv[3]
+    state_n = sys.argv[4]
+
+    try:
+        db = MySQLdb.connect(
+                host="localhost",
+                user=mysql_username,
+                password=mysql_password,
+                database=database_n,
+                port=3306
+                )
+        cursor = db.cursor()
+        sql_qy = "SELECT * FROM states WHERE name LIKE %s\
+                ORDER BY states.id ASC"
+        cursor.execute(sql_qy, (state_n, ))
+        results = cursor.fetchall()
+
+        for row in results:
+            print(row)
+        db.close()
+    except MySQLdb.Error as e:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
