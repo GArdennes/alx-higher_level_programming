1b4fd7e7ca504537457a36dfcb2175f16edf3248
GArdennes <kevin.afachao@gmail.com>
2023-12-01 11:31:13 -0800

    Task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Script sends request URL, and displays the response
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    response = requests.get(argv[1])
+    if response.status_code >= 400:
+        print("Error code: {}".format(
+            response.status_code))
+    else:
+        print(response.text)
