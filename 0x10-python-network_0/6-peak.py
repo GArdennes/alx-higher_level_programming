48b6beea02603b3456dd6ddc4f10637a0ee8e8d1
GArdennes <kevin.afachao@gmail.com>
2023-12-01 01:38:08 -0800

    Task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python3
 """
-Finds a peak integer in a lsit of unsorted integers
+Finds a peak in a list of unsorted integers
 """
 
 
 def find_peak(list_of_integers):
     """
-    Implements the modified binary sort algorithm
-    Returns the peak number or none
+    Args:
+        list_of_integers(int): list of integers to find peak of
+    Returns: peak of list_of_integers or None
     """
     if list_of_integers is None or not list_of_integers:
         return None
+
     start = 0
-    end = len(list_of_integers)-1
+    end = len(list_of_integers) - 1
     peak = None
     done = False
+
     while not done:
         if start <= end:
-            mid = (start+end)//2
-            if mid == start or\
-                    list_of_integers[mid] > list_of_integers[mid-1] and\
-                    if mid == end or\
-                    list_of_integers[mid] > list_of_integers[mid + 1]:
+            mid = (start + end) // 2
+
+            if (mid == 0 or list_of_integers[mid] > list_of_integers[mid - 1]) and \
+               (mid == end or list_of_integers[mid] > list_of_integers[mid + 1]):
                 peak = list_of_integers[mid]
                 done = True
             else:
-                if list_of_integers[mid] < list_of_integers[mid - 1]:
+                if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
                     end = mid - 1
                 else:
                     start = mid + 1
         else:
             done = True
+
     return peak
