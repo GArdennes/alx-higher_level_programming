915e1a5c0a42144eec8b7fc713e2444c2435671e
GArdennes <thekevin.afachao@gmail.com>
2024-01-04 03:19:51 -0800

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,22 @@
 #!/usr/bin/node
-/* Displays the status code of a GET request */
+/* Display the number of movie the character
+Wedge Antilles is present */
 
 const request = require('request');
-const url = process.argv[2];
+const apiUrl = process.argv[2];
 
-request(url, (err, response) => {
+request(apiUrl, (err, response, body) => {
   if (err) console.log(err);
-  else console.log('code:', response.statusCode);
+  else {
+    const results = JSON.parse(body).results;
+    let count = 0;
+
+    for (const result of results) {
+      const characters = result.characters;
+      if (characters.some(char => char.search('18') > 0)) {
+        count++;
+      }
+    }
+    console.log(count);
+  }
 });
