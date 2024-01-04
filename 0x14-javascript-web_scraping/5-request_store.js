060ac3914968a26a09a4db3673438282762e6174
GArdennes <thekevin.afachao@gmail.com>
2024-01-04 03:23:19 -0800

    Task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,10 +1,16 @@
 #!/usr/bin/node
-/* Displays the status code of a GET request */
+/* Stores the contents of a webpage in a file */
 
 const request = require('request');
+const fs = require('fs');
 const url = process.argv[2];
+const filename = process.argv[3];
 
-request(url, (err, response) => {
+request(url, (err, response, body) => {
   if (err) console.log(err);
-  else console.log('code:', response.statusCode);
+  else {
+    fs.writeFile(filename, body, 'utf8', (err) => {
+      if (err) console.log(err);
+    });
+  }
 });
