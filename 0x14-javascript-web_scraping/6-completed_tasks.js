458d1fd298d04b2903fb48337db2b270da4b79c7
GArdennes <thekevin.afachao@gmail.com>
2024-01-04 03:30:42 -0800

    Task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,10 +1,20 @@
 #!/usr/bin/node
-/* Displays the status code of a GET request */
+/*  computes the number of tasks completed by user id. */
 
 const request = require('request');
 const url = process.argv[2];
 
-request(url, (err, response) => {
+request(url, (err, response, body) => {
   if (err) console.log(err);
-  else console.log('code:', response.statusCode);
+  else {
+	  const resp = {};
+	  const json = JSON.parse(body);
+
+	  for (const task of json){
+		  if (task.completed){
+			  resp[task.userId] = (resp[task.userId] || 0) + 1;
+		  }
+	  }
+	  console.log(resp);
+  }
 });
